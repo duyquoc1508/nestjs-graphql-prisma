9ef46a72a3d9162c1887a7347808ae83d47b15ec
quocnd <duyquoc.nguyen.bk@gmail.com>
2022-03-23 15:32:51 +0700

    feat(socket.io): add redis adapter pub sub

--- src/events/events.gateway.ts
+++ src/events/events.gateway.ts
@@ -12,7 +12,7 @@ import { from, Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { Socket, Server } from 'socket.io';
 
-@WebSocketGateway(8080)
+@WebSocketGateway(8080) // allow options as {namespace: '/chat'} */
 export class EventsGateway implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect {
   @WebSocketServer() server: Server;
   private logger : Logger = new Logger('EventGateway');
@@ -33,5 +33,29 @@ export class EventsGateway implements OnGatewayInit, OnGatewayConnection, OnGate
   onEvent(client: any, data: any): Observable<WsResponse<number>> {
     this.server.emit('push', 'message')
     return from([1, 2, 3]).pipe(map((item) => ({ event: 'events', data: item })));
+  }
+
+  /**
+   * @param payload {room: 'string', message: 'string'}
+   */
+  @SubscribeMessage('msgToServer')
+  public handleMessage(client: Socket, payload: any): Boolean {
+    console.log('room', payload.room)
+    console.log('message', payload.message)
+    return this.server.to(payload.room).emit('msgToClient', payload.message);
+  }
+
+  @SubscribeMessage('joinRoom')
+  public joinRoom(client: Socket, room: string): void {
+    client.join(room);
+    this.server.to(room).emit('joinRoom', `Client: ${client.id} joined the chat`)
+    // client.emit('joinRoom', room)
+  }
+
+  @SubscribeMessage('leaveRoom')
+  public leaveRoom(client: Socket, room: string): void {
+    client.leave(room);
+    this.server.to(room).emit('leaveRoom', `Client: ${client.id} leaved the chat`)
+    // client.emit('leaveRoom', room) // emit to this client
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,18 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { RedisIoAdapter } from './adapters/redis-io.adapter';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: ['error', 'warn', 'debug', 'log']
   });
+
+  // Uncomment these lines to use the Redis adapter:
+  const redisIoAdapter = new RedisIoAdapter();
+  await redisIoAdapter.connectToRedis();
+  app.useWebSocketAdapter(redisIoAdapter)
+
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
